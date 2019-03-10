4df06c7e8bd6f1198b3ef6dc35fe0299cba3385d
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-03-10 20:59:25 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,61 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-String nome = 'Carlos';
 //imports others
-void main() {
-  Firestore.instance
-      .collection('mensagens')
-      .document()
-      .collection('arqmidia')
-      .document()
-      .setData(data);
-
-  runApp(MaterialApp(
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  ));
+void main() async {
+  runApp(MyApp());
 }
 
-class Home extends StatefulWidget {
+//definindo os temas
+final ThemeData KIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData KDefaultTheme =
+    ThemeData(primarySwatch: Colors.purple, accentColor: Colors.orange[400]);
+
+class MyApp extends StatelessWidget {
   final Widget child;
 
-  Home({Key key, this.child}) : super(key: key);
+  MyApp({Key key, this.child}) : super(key: key);
 
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat Flutter'),
-        centerTitle: true,
-        backgroundColor: Colors.lightBlueAccent[400],
+    return MaterialApp(
+      title: 'Chat Flutter',
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? KIOSTheme
+          : KDefaultTheme,
+      home: ChatScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  final Widget child;
+
+  ChatScreen({Key key, this.child}) : super(key: key);
+
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Chat App'),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(),
       ),
-      body: Column(),
     );
   }
 }
