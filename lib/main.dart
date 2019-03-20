bc69ecbb7b9086093c15f54c2708d59b680ab597
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-03-20 19:59:56 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 //imports flutter
 import 'dart:io';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 //imports others
 void main() async {
